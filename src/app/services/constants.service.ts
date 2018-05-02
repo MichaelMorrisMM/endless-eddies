9dc13bc1db5ccdbde5bf53b6fbe99eb46f5cfbde
Justin Mills <justin.mills@utah.edu>
2018-05-02 15:27:01 -0600

    removed hard coded http://localhost:8080

--- src/app/services/constants.service.ts
+++ src/app/services/constants.service.ts
@@ -6,7 +6,7 @@ export class ConstantsService {
     constructor() {
     }
 
-    public static readonly URL_PREFIX: string = 'http://localhost:8080/endless-eddies';
+    public static readonly URL_PREFIX: string = '/endless-eddies';
     public readonly DIRTY_NOTE_MESSAGE: string = 'Changes have not been saved';
     public static readonly VALIDATOR_TYPE_REQUIRED: string = "required";
     public static readonly VALIDATOR_TYPE_MIN: string = "min";
