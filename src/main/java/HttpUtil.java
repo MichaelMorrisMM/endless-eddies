fc932c8205cdcf6bb6b5a1b8855a9d770e3892a5
MichaelMorrisMM <michaelmorrismm@gmail.com>
2018-05-06 14:17:37 -0600

    Removed CORS stuff from back-end. (mmorris)

--- src/main/java/HttpUtil.java
+++ src/main/java/HttpUtil.java
@@ -8,24 +8,12 @@ import java.io.PrintWriter;
 
 public class HttpUtil {
 
-    private static final boolean ALLOW_CORS_LOCALHOST = true;
-    private static final String LOCALHOST_DEV_SERVER = "http://localhost:4200";
-
     private static final String CONTENT_TYPE_MIME_JSON = "application/json";
 
     public static void doGetSetup(HttpServletResponse response) {
-        setCORSHeaders(response);
     }
 
     public static void doPostSetup(HttpServletResponse response) {
-        setCORSHeaders(response);
-    }
-
-    private static void setCORSHeaders(HttpServletResponse response) {
-        if (ALLOW_CORS_LOCALHOST) {
-            response.setHeader("Access-Control-Allow-Origin", LOCALHOST_DEV_SERVER);
-            response.setHeader("Vary", "Origin");
-        }
     }
 
     public static void printJSONResponse(HttpServletResponse response, JsonObject o) throws IOException {
