6eb13b1c4dcdbd73836eede3ab339b03dabcd123
Justin Mills <justin.mills@utah.edu>
2018-08-28 06:50:34 -0600

    added marked to the home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ConfiguratorService } from '../services/configurator.service';
 import {Config} from "../configurator/config.interface";
+import * as marked from 'marked';
 
 @Component({
   selector: 'app-home',
@@ -16,9 +17,11 @@ import {Config} from "../configurator/config.interface";
 })
 export class HomeComponent implements OnInit {
   config: Config;
+  pageContent: string;
   constructor(public configuratorService: ConfiguratorService) {
   }
   ngOnInit() {
     this.configuratorService.getConfiguration().subscribe( con => this.config = con );
+    this.pageContent = marked.parse('# Home\n\nThis is a test of marked.js\n\n**marked**');
   }
 }
