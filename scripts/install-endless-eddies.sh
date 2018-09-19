4483be0972bd482b334fdd4189856477da4452ce
Justin Mills <justin.mills@utah.edu>
2018-09-18 21:04:42 -0600

    update the install script

--- scripts/install-endless-eddies.sh
+++ scripts/install-endless-eddies.sh
@@ -7,13 +7,13 @@ sudo apt-get -y install openjdk-8-jdk-headless
 
 # install tomcat 9
 cd $HOME
-wget ftp://apache.cs.utah.edu/apache.org/tomcat/tomcat-9/v9.0.7/bin/apache-tomcat-9.0.7.tar.gz
-tar xzf apache-tomcat-9.0.7.tar.gz
-mv apache-tomcat-9.0.7 apache-tomcat-9
+wget ftp://apache.cs.utah.edu/apache.org/tomcat/tomcat-9/v9.0.12/bin/apache-tomcat-9.0.12.tar.gz
+tar xzf apache-tomcat-9.0.12.tar.gz
+mv apache-tomcat-9.0.12 apache-tomcat-9
 export CATALINA_HOME=$HOME"/apache-tomcat9"
 
 # install node.js and npm
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get -y install nodejs
 
 # clone and build endless-eddies
@@ -24,4 +24,5 @@ cd endless-eddies
 mkdir conf
 export ENDLESS_EDDIES_CONFIG_DIR=$HOME"/endless-eddies/conf"
 chmod u+x gradlew
-./gradlew deployAll
+./gradlew makeDatabase
+./gradlew deployWar
