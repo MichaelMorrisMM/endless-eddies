72909dcc357b068ec12312466a5c21e3c2dcd453
Justin Mills <justin.mills@utah.edu>
2018-11-06 21:45:26 -0700

    added katex lib for displaying math equations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {ConfiguratorService} from '../services/configurator.service';
-import {Config} from '../configurator/config.interface';
 import * as marked from 'marked';
-import {DomSanitizer} from "@angular/platform-browser";
+import {DomSanitizer} from '@angular/platform-browser';
+import {HomePage} from '../configurator/home-page.model';
+import * as katex from 'katex';
 
 @Component({
     selector: 'app-home',
@@ -11,14 +12,24 @@ import {DomSanitizer} from "@angular/platform-browser";
     `]
 })
 export class HomeComponent implements OnInit {
-    public config: Config;
     public md = marked.setOptions({});
+    public parsedHomePages: HomePage[] = [];
 
     constructor(public configuratorService: ConfiguratorService,
                 public sanitizer: DomSanitizer) {
     }
 
+    private static equationReplacer(match: string, equation: string): string {
+        return katex.renderToString(equation, {
+            throwOnError: false
+        });
+    }
+
     ngOnInit() {
-        this.configuratorService.getConfiguration().subscribe(con => this.config = con);
+        this.configuratorService.getConfiguration().subscribe(config => config.homePages.forEach(page => {
+                page.content = this.md.parse(page.content).replace(/\$\$(.+)\$\$/, HomeComponent.equationReplacer);
+                this.parsedHomePages.push(page);
+            }
+        ));
     }
 }
