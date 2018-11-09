998e5d5410ed927b3ce0a050bfc3d7694b7857aa
Justin Mills <justin.mills@utah.edu>
2018-11-08 19:48:04 -0700

    udpate equation replacement regex

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,7 +27,7 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
         this.configuratorService.getConfiguration().subscribe(config => config.homePages.forEach(page => {
-                page.content = this.md.parse(page.content).replace(/\$\$(.+)\$\$/, HomeComponent.equationReplacer);
+                page.content = this.md.parse(page.content).replace(/\$\$(.+?)\$\$/g, HomeComponent.equationReplacer);
                 this.parsedHomePages.push(page);
             }
         ));
